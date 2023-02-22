391be954b4718d76922ac03a58f47162bd7eefed
Avery Brinkman <averybrinkman@gmail.com>
2023-02-22 08:36:33 -0500

    check response and disconnect

--- Assignment1/FtpClient.py
+++ Assignment1/FtpClient.py
@@ -15,18 +15,22 @@ class FtpClient:
   def connect(self, username: str, password: str):
     self.controlSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.controlSock.connect((self.ftpAddr, self.ftpPort))
-    serverResponse = self.controlSock.recv(1024)
+    assert int(self.controlSock.recv(1024)[:3]) == 220
 
-    self.sendCommand("USER", username)
-    self.sendCommand("PASS", password)
+    self.sendCommand("USER " + username, 331)
+    self.sendCommand("PASS " + password, 230)
 
-    self.controlSock.close() 
+    self.disconnect()
 
-  def sendCommand(self, command: str, arg: str):
-    fullCommand = bytes(command, "utf-8") + b' ' + bytes(arg, "utf-8") + CLRF
+  def disconnect(self):
+    self.sendCommand("QUIT", 221)
+    self.controlSock.close()
+
+  def sendCommand(self, command: str, expectedResponse: int):
+    fullCommand = bytes(command, "utf-8") + CLRF
     self.controlSock.send(fullCommand) 
     serverResponse = self.controlSock.recv(1024)
-    print(serverResponse)
+    assert int(serverResponse[:3]) == expectedResponse
 
 a = FtpClient()
 a.connect("abrinkman", "admin123")
